e0d79da70e55053a2e494ebea9a1c1cf2e8215b5
Владислав Ежов <v.ezhov@qsoft.ru>
2023-03-07 23:11:23 +0300

    update bot's structure

--- auth.py
+++ auth.py
@@ -1,3 +1,2 @@
-
 def auth(message):
     return "Гуд"
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import os
 import telebot
 from telebot import types
+from dotenv import load_dotenv
 
-import auth
+from auth import auth
 
-# bot = telebot.TeleBot(os.getenv("TOKEN"))
-bot = telebot.TeleBot("")
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+bot = telebot.TeleBot(os.environ.get("TELEGRAM_TOKEN"))
 
 @bot.message_handler(commands=['start']) #стартовая команда
 def start(message):
@@ -12,12 +16,11 @@ def start(message):
     markup.add(types.KeyboardButton("Отправить контакт", request_contact=True))
     bot.send_message(message.from_user.id, "Привет! Здесь ты сможешь обменять свои CTFCoin'ы на наш мерч;) Но сначала нужно авторизоваться, пришли в ответ свой контакт", reply_markup=markup)
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text','contact'])
 def get_text_messages(message):
-    print(message)
-
     if message.contact is not None:
         answer = auth(message)
-        bot.send_message(message.from_user.id, answer)
+        bot.send_message(message.from_user.id, answer, reply_markup=types.ReplyKeyboardRemove())
+
 
 bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
